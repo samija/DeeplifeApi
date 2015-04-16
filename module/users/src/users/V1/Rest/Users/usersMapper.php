73dfc122035e959eef7c6b7276198d1e8412c235
Frezer Tadesse <frezertade@gmail.com>
2015-04-16 19:06:50 +0300

    the get method with view table and some trying mysql in the creat method

--- module/users/src/users/V1/Rest/Users/usersMapper.php
+++ module/users/src/users/V1/Rest/Users/usersMapper.php
@@ -69,7 +69,7 @@ class UsersMapper
          $data= (array) $data;
        $ph_no =$data['Phone_No'];
         $sql= "INSERT INTO `users` (`User_Id`,`First_Name`,`Last_Name`,`User_Name`,`Email`,`Phone_No`,`Password`)
-                      VALUES ('user','$data[First_Name]','$data[Last_Name]','$data[User_Name]','$data[Email]','$data[Phone_No]','$data[Password]')";
+                      VALUES ('$data[User_Id]','$data[First_Name]','$data[Last_Name]','$data[User_Name]','$data[Email]','$data[Phone_No]','$data[Password]')";
         $sql_u_id= 'SELECT `users`.`User_Id` FROM users
                           WHERE `Phone_No`= ?';
               $userID= $this->adpter->query($sql_u_id, array($ph_no));
